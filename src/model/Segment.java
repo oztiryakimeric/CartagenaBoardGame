35ad58e0f1d5d310953a9500ea808c516a811c02
Meriç <oztiryakimeric@gmail.com>
2018-01-12 23:55:02 +0300

    Minor bug fixes

--- src/model/Segment.java
+++ src/model/Segment.java
@@ -11,20 +11,6 @@ public class Segment {
         this.index = index;
     }
 
-    public GameCell findPossibleForwardCell(int startingPoint, Symbol symbol){
-        for(int i=startingPoint; i<cells.length; i++)
-            if(cells[i].getSymbol().equals(symbol) && cells[i].getPirateCount() == 0)
-                return cells[i];
-        return null;
-    }
-
-    public GameCell findPossibleBackwardCell(int startingPoint){
-        for(int i=startingPoint - 1; i>=0; i--)
-            if(cells[i].getPirateCount() > 0 && cells[i].getPirateCount() < 3)
-                return cells[i];
-        return null;
-    }
-
     public GameCell[] getCells() {
         return cells;
     }
